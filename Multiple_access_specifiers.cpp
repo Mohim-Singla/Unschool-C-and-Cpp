6bd63c090dc3ffbbbd65c103b8cb5d4bb290067d
Mohim-Singla <69247094+Mohim-Singla@users.noreply.github.com>
2020-08-05 21:31:39 +0530

    Add files via upload

---
+++ Multiple_access_specifiers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    printf("Enter three numbers: ");
+    int first_num, second_num, third_num;
+    scanf("%d" "%d" "%d",&first_num,&second_num,&third_num);
+    printf("Sum of %d, %d and %d is %d.",first_num, second_num, third_num,first_num+second_num+third_num);
+    return 0;
+}
